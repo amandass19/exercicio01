2c0eefc7f22225d54390128a4678b71d579c08c3
Amanda e Paolla <paollafermendes@gmail.com>
2022-02-18 22:04:45 -0300

    exercicio

---
+++ Calculadora.java
@@ -0,0 +1,15 @@
+public static void main(String[]args){
+
+    int numero1;
+    int numero2;
+
+    String op="";
+    String resultado=0;
+
+    System.out.println("Digite a operação: + / -");
+    op=entrada.nextLine;
+
+    
+
+
+}
